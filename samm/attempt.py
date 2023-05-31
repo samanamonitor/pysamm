9690220b3964d3b1f096dfbde0d605fac724d143
Fabian Baena <fabbaena@gmail.com>
2023-05-31 08:57:29 -0400

    added metric and tag classes

--- samm/attempt.py
+++ samm/attempt.py
@@ -1,5 +1,32 @@
 import time
 
+class Tag:
+    def __init__(self, key, value):
+        self.key=key
+        self.value=value
+    def __str__(self):
+        return "%s=\"%s\"" % (self.key, self.value)
+
+class InstanceMetric:
+    def __init__(self, name, value, tags=[], prefix=None):
+        if isinstance(prefix, str):
+            self._name = "%s_%s" % (prefix, name)
+        else:
+            self._name=name
+        self.value=value
+        self._tags=tags.copy()
+    def add_tag(self, tag):
+        self._tags += [tag]
+    @property
+    def tags(self):
+        return ",".join([ str(x) for x in self._tags ])
+    @property
+    def key(self):
+        return "%s{%s}" % (self._name, self.tags)
+    def __str__(self):
+        return "%s %s" % (
+            self.key, self.value)
+
 class Attempt:
     def __init__(self, config, instance_name, check_name):
         self.check=config.get("checks." + check_name)
@@ -22,12 +49,26 @@ class Attempt:
     def run(self, metric_data):
         if time.time() < self.next_run:
             return False
-        for d in self.data:
-            for m in self.metrics:
-                tags=[ "instance=\"%s\"" % self.instance_name.lower() ]
-                for t in self.tag_property:
-                    tags += [ "%s=\"%s\"" % (t, d[t])]
-                metric_data["%s_%s{%s}" % (self.alias.lower(), m.lower(), ",".join(tags).lower())] = d[m]
+        instance_name=self.instance_name.lower()
+        base_tags = [ Tag("instance", instance_name), Tag("job", "windows") ]
+        if instance_name not in metric_data:
+            metric_data[instance_name] = {}
+
+        instance_metric = metric_data[instance_name]
+        im_up = InstanceMetric("up", 1, base_tags)
+        try:
+            instance_metric[im_up.key] = im_up
+            for d in self.data:
+                for m in self.metrics:
+                    im = InstanceMetric(m.lower(), d[m], base_tags, prefix=self.alias.lower())
+                    for t in self.tag_property:
+                        im.add_tag(Tag(t.lower(), d[t].lower()))
+                    instance_metric[im.key] = im
+        except Exception as e:
+            print(e)
+            im_up.value = 0
+            metric_data[instance_name] = { im_up.key:  im_up }
+
         self.next_run = 0
         return True
 
