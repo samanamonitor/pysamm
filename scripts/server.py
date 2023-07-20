145d714f90587aa985be1cf5bcf2d03809304e05
Fabian Baena <fabian.baena@samanagroup.com>
2023-07-20 15:07:28 +0000

    added an idle timeout to prevent container to die immediately and allow admin to get in a try to fix

--- scripts/server.py
+++ scripts/server.py
@@ -2,13 +2,19 @@
 import sys
 from sys import argv
 from samm.service import Service
+import time
 
 svc = None
 
 def main(config_file):
     global svc
-    svc = Service(config_file)
-    svc.process_loop()
+    try:
+        svc = Service(config_file)
+        svc.process_loop()
+    except Exception as e:
+        print(e)
+        time.sleep(600)
+        raise
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
