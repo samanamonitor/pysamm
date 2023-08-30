d5cec2d9dfd6364ba43042a84eb8f12f4d867619
Fabian Baena <fabian.baena@samanagroup.com>
2023-08-30 17:21:04 -0400

    added flask server to remove apache from samm deployment. This allows for standalone containers for each samm

--- scripts/server.py
+++ scripts/server.py
@@ -3,17 +3,49 @@ import sys
 from sys import argv
 from samm.service import Service
 import time
+import threading
+from flask import Flask, make_response
+import socket
+
+app = Flask(__name__)
 
 svc = None
 
+@app.route('/metrics')
+def metrics():
+    global svc
+    address = svc.running_config.get("sock_file")
+    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    s.connect(address)
+    s.send(b'\n')
+    data = ""
+    while True:
+        newdata = s.recv(1024).decode('ascii')
+        if len(newdata) == 0:
+            break
+        data += newdata
+
+    s.close()
+    res = make_response(data, 200)
+    res.mimetype = "text/plain"
+    return res
+
+def web(port=5000):
+    app.run(host='0.0.0.0', port=port)
+
 def main(config_file):
     global svc
+    wait_on_error=0
     try:
         svc = Service(config_file)
+        wait_on_error = svc.running_config.get("wait_on_error", 0)
+        if svc.running_config.get("webserver", False):
+            port = svc.running_config.get("webserver_port", 5000)
+            threading.Thread(target=web, args=[port], daemon=True).start()
         svc.process_loop()
     except Exception as e:
         print(e)
-        time.sleep(60)
+        time.sleep(wait_on_error)
         raise
 
 if __name__ == "__main__":
