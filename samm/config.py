83cf6968df9192897e649a99993ea4db49d39114
Fabian Baena <fabian.baena@samanagroup.com>
2023-08-31 14:40:20 -0400

    moved functions get_variables and replace_vars out of the class; Start using objecttype classes instead of dicts; improve exceptions for testing

--- samm/config.py
+++ samm/config.py
@@ -1,52 +1,116 @@
 import re, os, json, sys
-from .instance import Instance
+from .objecttype import Instance, Command, Check
+
+def get_variables(s):
+    '''This function receives a string containing text and variables in the form $(variable_name)
+    and will return a list of variable names. It can detect multiple variables.
+    Variable names cannot contain the following characters (,),\\ or quotes
+'''
+    var_regex=r'(?<!\\)\$\(([^\)]+)\)'
+    varlist=re.findall(var_regex, s)
+    return varlist
+
+def replace_vars(o, config, instance_name=None, check_name=None, default=None, default_variable=""):
+    out=None
+    if isinstance(o, str):
+        '''
+        We don't need to continue recursion. Just find variables and replace them
+        '''
+        variables=get_variables(o)
+        out=o
+        if not isinstance(default, str):
+            defaut = ""
+        for v in variables:
+            var_value=config.get(v, instance_name=instance_name, check_name=check_name, resolve_vars=True, default=default)
+            if isinstance(var_value, str):
+                out=o.replace("$(%s)" % v, var_value)
+            elif o == "$(%s)" % v:
+                out=var_value
+            else:
+                raise TypeError("Unable to parse variable %s." % o)
+    elif isinstance(o, list):
+        '''
+        We need to process recursively for each item in the list
+        '''
+        out = [None] * len(o)
+        for i in range(len(o)):
+            out[i] = replace_vars(o[i], config, instance_name=instance_name, check_name=check_name, default=default)
+    elif isinstance(o, dict):
+        '''
+        We need to process recursively for each item in the dict
+        '''
+        out = {}
+        for i in o:
+            out[i] = replace_vars(o[i], config, instance_name=instance_name, check_name=check_name)
+    else:
+        out = default
+    return out
+
 
 class Config():
     def __init__(self, config_file):
         self._valid_config = False
         self.config_file = config_file
         self.modules = {}
-        self.reload()
+        self._config = None
 
     def reload(self):
         with open(self.config_file) as f:
             self._config = json.load(f)
+
+        if "base_dir" not in self._config:
+            raise KeyError("missing base_dir")
+        if not os.path.exists(self._config['base_dir']):
+            raise FileNotFoundError("Directory not found. base_dir=%s" % self._config['base_dir'])
+
+        if "config_dir" not in self._config:
+            raise KeyError("missing config_dir")
+        self._config['config_dir'] = os.path.join(self._config['base_dir'], self._config['config_dir'])
+        if not os.path.exists(self._config['config_dir']):
+            raise FileNotFoundError("Directory not found config_dir=%s" % self._config['config_dir'])
+
         if "resource_file" not in self._config:
-            raise TypeError("missing resource_file")
-        self.resource_file_path = self._config['base_dir'] + "/" + self._config['config_dir'] + "/" + self._config['resource_file']
+            raise KeyError("missing resource_file")
+
+        self.resource_file_path = os.path.join(self._config['config_dir'], self._config['resource_file'])
         with open(self.resource_file_path) as f:
             self._config["resources"] = json.load(f)
+
         self._config["commands"] = {}
         self._config["checks"] = {}
         self._config["instances"] = {}
+
+        object_files = self._config.get('object_files', [])
+        if not isinstance(object_files, list):
+            raise TypeError("object_files must be a list")
         for c in self._config['object_files']:
-            object_file_path = self._config['base_dir'] + "/" + self._config['config_dir'] + "/" + c
+            object_file_path = os.path.join(self._config['config_dir'], c)
             self.load(object_file_path)
+
         _ = self._config.setdefault("tags", {}).setdefault("job", "samm")
         self._valid_config = True
+        return self._valid_config
 
 
     def load(self, filename):
-        c=""
+        c=None
         with open(filename) as f:
             c=json.load(f)
         if not isinstance(c, list):
             raise TypeError("Invalid config type. Must be list")
         for o in c:
             if not isinstance(o, dict):
-                raise TypeError("Invalid config type. data=%s" % str(o))
+                raise TypeError("Invalid config type. Expecting dict. data=%s" % str(o))
             if "object_type" not in o:
-                raise TypeError("Invalid object_type. data=%s" % str(o))
+                raise KeyError("Mandatory \'object_type\' missing. data=%s" % str(o))
             if "name" not in o:
-                raise TypeError("Invalid object data. data=%s" % str(o))
+                raise KeyError("Mandatory \'name\' missing. data=%s" % str(o))
             if o["object_type"] == "command":
-                self._config["commands"][o["name"]] = o
+                self._config["commands"][o["name"]] = Command(o, self._config)
             elif o["object_type"] == "check":
-                self._config["checks"][o["name"]] = o
+                self._config["checks"][o["name"]] = Check(o, self._config)
             elif o["object_type"] == "instance":
-                self._config["instances"][o["name"]] = Instance(o)
-            elif o["object_type"] == "check_group":
-                self._config["check_groups"][o["name"]] = o
+                self._config["instances"][o["name"]] = Instance(o, self._config)
 
     def setdefault(self, key, value):
         return self._config.setdefault(key, value)
@@ -76,48 +140,7 @@ class Config():
                 return default
             return curconfig
 
-        return self.replace_vars(curconfig, instance_name=instance_name, check_name=check_name, default=default, default_variable=default_variable)
-
-    def replace_vars(self, o, instance_name=None, check_name=None, default=None, default_variable=""):
-        out=None
-        if isinstance(o, str):
-            '''
-            We don't need to continue recursion. Just find variables and replace them
-            '''
-            variables=self._get_variables(o)
-            out=o
-            if not isinstance(default, str):
-                defaut = ""
-            for v in variables:
-                var_value=self.get(v, instance_name=instance_name, check_name=check_name, resolve_vars=True, default=default)
-                if isinstance(var_value, str):
-                    out=o.replace("$(%s)" % v, var_value)
-                elif o == "$(%s)" % v:
-                    out=var_value
-                else:
-                    raise TypeError("Unable to parse variable %s." % o)
-        elif isinstance(o, list):
-            '''
-            We need to process recursively for each item in the list
-            '''
-            out = [None] * len(o)
-            for i in range(len(o)):
-                out[i] = self.replace_vars(o[i], instance_name=instance_name, check_name=check_name, default=default)
-        elif isinstance(o, dict):
-            '''
-            We need to process recursively for each item in the dict
-            '''
-            out = {}
-            for i in o:
-                out[i] = self.replace_vars(o[i], instance_name=instance_name, check_name=check_name)
-        else:
-            out = default
-        return out
-
-    def _get_variables(self, s):
-        var_regex=r'(?<!\\)\$\(([^\)]+)\)'
-        varlist=re.findall(var_regex, s)
-        return varlist
+        return replace_vars(curconfig, self, instance_name=instance_name, check_name=check_name, default=default, default_variable=default_variable)
 
     def run_module(self, module_str, **kwargs):
         if module_str not in self.modules:
