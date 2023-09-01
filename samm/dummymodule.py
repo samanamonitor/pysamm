22f43b0bdb2617460d58a574798ac116b4835379
Fabian Baena <fabian.baena@samanagroup.com>
2023-09-01 11:52:14 -0400

    added dummy up and down modules for tests

---
+++ samm/dummymodule.py
@@ -0,0 +1,15 @@
+class DummyModule:
+	def __iter__(self):
+		return iter(self.data)
+
+
+class DummyModuleUp(DummyModule):
+	def __init__(self, *args, **kwargs):
+		self.data = [ 
+		{ "name": "item1", "metric1": 1, "metric2": 10}, 
+		{ "name": "item2", "metric1": 2, "metric2": 20}, 
+		{ "name": "item3", "metric1": 3, "metric2": 30} ]
+
+class DummyModuleDown(DummyModule):
+	def __init__(self, *args, **kwargs):
+		self.data = None
