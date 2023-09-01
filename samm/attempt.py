5ac46059b555531aeacf28285026b00b99d297e5
Fabian Baena <fabian.baena@samanagroup.com>
2023-09-01 11:49:37 -0400

    removed Tag class and organize tags in dicts; fix up metric value for when no metrics are received from instance

--- samm/attempt.py
+++ samm/attempt.py
@@ -1,13 +1,6 @@
 from threading import Thread
 from .metric import InstanceMetric
-
-class Tag:
-    def __init__(self, key, value):
-        self.key=key
-        self.value=value
-    def __str__(self):
-        return "%s=\"%s\"" % (self.key, self.value)
-
+import time
 
 class Attempt:
     def __init__(self, config, instance_name, check_name):
@@ -19,8 +12,8 @@ class Attempt:
         self.instance_name=instance_name
         self.check_name = check_name
         self.alias = self.check.get('alias', check_name)
-        self.tag_property = config.get(("checks", check_name, "tag_property"), resolve_vars=True, default=[])
-        self.command="commands", config.get(("checks", check_name, "command"), resolve_vars=True)
+        self.command="commands", config.get(("checks", check_name, "command"), \
+            resolve_vars=True)
         self.command_args=config.get(self.command + ("args",), 
             instance_name=instance_name, check_name=check_name, resolve_vars=True)
         self.module_name=config.get(self.command + ("type",), 
@@ -30,15 +23,13 @@ class Attempt:
         self.thread = None
         self.instance_stale_timeout = config.get(("instances", instance_name, "stale_timeout"))
         self.check_stale_timeout = config.get(("checks", check_name, "stale_timeout"))
-        self.base_tags = []
-        self.add_base_tags(config.get(("tags"), {}))
-        self.add_base_tags(config.get(("instances", instance_name, "tags"), default={}))
-        self.add_base_tags(config.get(("checks", check_name, "tags"), default={}))
-        self.add_base_tags({ "instance": self.instance_name })
-
-    def add_base_tags(self, tags):
-        for key in tags:
-            self.base_tags += [ Tag(key, tags[key]) ]
+        self.tag_properties = config.get(("checks", check_name, "tag_properties"), \
+                    resolve_vars=True, default=[])
+        self.base_tags = {}
+        self.base_tags.update(config.get(("tags"), default={}))
+        self.base_tags.update(config.get(("instances", instance_name, "tags"), default={}))
+        self.base_tags.update(config.get(("checks", check_name, "tags"), default={}))
+        self.base_tags['instance'] = self.instance_name
 
     def due(self):
         if self.next_run == 0:
@@ -53,29 +44,31 @@ class Attempt:
         return True
 
     def run(self, metric_data, schedule_next=True):
+        self.next_run = 0
         instance_name=self.instance_name.lower()
         instance_metric = metric_data.setdefault(instance_name, {})
 
-        im_up = InstanceMetric("up", 1, self.base_tags, stale_timeout=self.instance_stale_timeout)
+        im_up = InstanceMetric("up", 0, self.base_tags, stale_timeout=self.instance_stale_timeout)
+        instance_metric[im_up.key] = im_up
         try:
-            instance_metric[im_up.key] = im_up
-            for d in self.data:
-                for m in self.metrics:
-                    if m not in d:
-                        continue
-                    im = InstanceMetric(m.lower(), d[m], self.base_tags, prefix=self.alias.lower(), stale_timeout=self.check_stale_timeout)
-                    for t in self.tag_property:
-                        im.add_tag(Tag(t.lower(), str(d[t]).lower()))
+            metric_received = 0
+            for metric_data in self.data:
+                metric_received = 1
+                metric_tags = self.base_tags.copy()
+                for tag_property in self.tag_properties:
+                    metric_tags.update({tag_property.lower(): str(metric_data.get(tag_property, "none").lower())})
+
+                for metric_name in self.metrics:
+                    im = InstanceMetric(metric_name.lower(), metric_data.get(metric_name, -1), metric_tags, \
+                        prefix=self.alias.lower(), stale_timeout=self.check_stale_timeout)
                     instance_metric[im.key] = im
+            im_up.val(metric_received)
         except Exception as e:
-            im_up.value = 0
-            metric_data[instance_name] = { im_up.key:  im_up }
-            raise
+            pass
 
         if schedule_next:
             self.schedule_next()
-        else:
-            self.next_run = 0
+
         return True
 
     def schedule_next(self):
