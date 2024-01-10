0a24cee5ace83a959327542bd7d870e661fc3ca1
Fabian Baena <fabian.baena@samanagroup.com>
2024-01-10 07:18:59 -0500

    Added LDAPFilter class that allows for dict to string functionality

--- samm/utils.py
+++ samm/utils.py
@@ -4,6 +4,8 @@ import logging
 
 log = logging.getLogger(__name__)
 
+
+
 class LokiStream:
     def __init__(self, submodule_str, loki_url, tags={}, tag_property={}, time_property=None, **kwargs):
         mod_str, _sep, class_str = submodule_str.rpartition('.')
@@ -70,3 +72,60 @@ class LokiStream:
             raise StopIteration
         self._sent = 1
         return { **self.metrics, **self.tags }
+
+
+class FilterFunction:
+    def adsecondsfromnow(seconds):
+        if not isinstance(seconds, int):
+            seconds = int(seconds)
+        return int(time() - seconds + 11644473600) * 10000000
+
+    def lastlogon_to_timestamp(ll):
+        if isinstance(ll, list) and len(ll) > 0:
+            ll = ll[0]
+        if isinstance(ll, bytes):
+            ll = ll.decode('ascii')
+        return (int(ll) / 10000000) - 11644473600
+
+    def filter_dict_to_string(filter_dict):
+        '''This function converts dictionary to ldap filter string.
+        TODO: allow for more complex expressions. Now only accepts AND for all
+              elements in the expression
+        '''
+        if not isinstance(filter_dict, dict):
+            raise TypeError("Invalid filter. Must be dictionary")
+        operator_mapping = {
+            "eq": "=",
+            "le": "<=",
+            "ge": ">=",
+            "px": "~="
+        }
+        filterlist = []
+        for attribute, value in filter_dict.items():
+            if not isinstance(value, dict):
+                raise TypeError("Invalid filter expression. Value must be a dictionary. %s" % filter_dict)
+            filterstr = attribute
+            try:
+                op, value = next(iter(value.items()))
+            except StopIteration:
+                raise TypeError("Invalid filter expression. Value dictionary must have at least one element. %s" \
+                    % filter_dict)
+            try:
+                filterstr += operator_mapping[op]
+            except KeyError:
+                raise TypeError("Invalid filter expression. Only values allowed for operator are: eq, ge, le, px. %s" \
+                     % filter_dict)
+            if isinstance(value, dict):
+                func_name, param = next(iter(value.items()))
+                if func_name[:4] != "Fn::":
+                    raise TypeError("Expecting function. Use syntax Fn::<function name>. %s" % filter_dict)
+                func_name = func_name[4:]
+                func = getattr(FilterFunction, func_name)
+                value = func(*param)
+            filterstr += str(value)
+            filterlist += [ "(%s)" % filterstr ]
+        if len(filterlist) < 1:
+            raise TypeError("Invalid filter. Dictonary cannot be empty. %s" % filter_dict)
+        if len(filterlist) > 1:
+            return "(&%s)" % "".join(filterlist)
+        return filterlist[0]
