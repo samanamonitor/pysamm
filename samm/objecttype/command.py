d2b28fe131a141d8ca42c19aa57ef41b6295786e
Fabian Baena <fabian.baena@samanagroup.com>
2024-01-15 15:33:43 -0500

    separate class initialization tasks with processing tasks, to allow object refresh using a reprocess function

--- samm/objecttype/command.py
+++ samm/objecttype/command.py
@@ -3,6 +3,9 @@ from .sammobject import SammObject
 class Command(SammObject):
 	def __init__(self, object_definition, configuration=None):
 		super(Command, self).__init__(object_definition, configuration)
+
+	def pre_process(self):
+		super(Command, self).pre_process()
 		self._config_section = "commands"
 
 	def post_process(self):
