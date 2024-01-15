883704985c7f6e42075eb540c4a48304d919f481
Fabian Baena <fabian.baena@samanagroup.com>
2024-01-15 09:39:30 -0500

    add test for multiple definition of objects and merging

---
+++ tests/ldap_discovery_data2.py
@@ -0,0 +1,12 @@
+[
+	(100, [('CN=CTXNSTF01,OU=CONTROL,OU=CTX,OU=Servers,DC=domain,DC=local', {'cn': [b'CTXNSTF01'], 'dNSHostName': [b'ctxnstf01.domain.local']})]),
+	(100, [('CN=CTXNDDC01,OU=CONTROL,OU=CTX,OU=Servers,DC=domain,DC=local', {'cn': [b'CTXNDDC01'], 'dNSHostName': [b'ctxnddc01.domain.local']})]),
+	(100, [('CN=CTXNLIC01,OU=CONTROL,OU=CTX,OU=Servers,DC=domain,DC=local', {'cn': [b'CTXNLIC01'], 'dNSHostName': [b'ctxnlic01.domain.local']})]),
+	(100, [('CN=CTXNDDC02,OU=CONTROL,OU=CTX,OU=Servers,DC=domain,DC=local', {'cn': [b'CTXNDDC02'], 'dNSHostName': [b'ctxnddc02.domain.local']})]),
+	(100, [('CN=CTXNFED01,OU=CONTROL,OU=CTX,OU=Servers,DC=domain,DC=local', {'cn': [b'CTXNFED01'], 'dNSHostName': [b'ctxnfed01.domain.local']})]),
+	(100, [('CN=CTXNSTF02,OU=CONTROL,OU=CTX,OU=Servers,DC=domain,DC=local', {'cn': [b'CTXNSTF02'], 'dNSHostName': [b'ctxnstf02.domain.local']})]),
+	(115, [(None, ['ldap://ForestDnsZones.domain.local/DC=ForestDnsZones,DC=domain,DC=local'])]),
+	(115, [(None, ['ldap://DomainDnsZones.domain.local/DC=DomainDnsZones,DC=domain,DC=local'])]),
+	(115, [(None, ['ldap://domain.local/CN=Configuration,DC=domain,DC=local'])]),
+	(101, [])
+]
