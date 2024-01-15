bdf7004403d55e58b25d92f7c5800b982fffa885
Fabian Baena <fabian.baena@samanagroup.com>
2024-01-15 09:23:50 -0500

    refactor post_process; change indentation to tabs

--- samm/objecttype/command.py
+++ samm/objecttype/command.py
@@ -1,11 +1,10 @@
 from .sammobject import SammObject
 
 class Command(SammObject):
-    def __init__(self, object_definition, configuration=None):
-        super(Command, self).__init__(object_definition, configuration)
-        self._config_section = "commands"
+	def __init__(self, object_definition, configuration=None):
+		super(Command, self).__init__(object_definition, configuration)
+		self._config_section = "commands"
 
-    def dict(self):
-        output = self._attributes.copy()
-        output['register'] = self.register
-        return output
+	def post_process(self):
+		super(Command, self).post_process()
+		self._applied_templates = True
