7f20e5590d32f4f1ab72615ec190a12716bd2b22
Fabian Baena <fabian.baena@samanagroup.com>
2024-01-15 09:20:02 -0500

    change indentantion to tabs; allow multiple definitions for the same object and it will merge all definitions

--- samm/objecttype/check.py
+++ samm/objecttype/check.py
@@ -1,22 +1,24 @@
 from .sammobject import SammObject, SammException
 
 class Check(SammObject):
-    def __init__(self, config, configuration=None):
-        super(Check, self).__init__(config, configuration)
-        self._config_section = "checks"
-        self._command = None
+	def __init__(self, config, configuration=None):
+		super(Check, self).__init__(config, configuration)
+		self._config_section = "checks"
+		self._command = None
 
-    def _post_process_internal(self):
-        command_name = self._attributes['command']
-        command = self._config.get(("commands", command_name))
-        if command is None:
-            raise SammException("Command %s defined in check %s doesn't exist" % 
-                (self.command, self.name))
-        command.post_process()
-        self._command = command
+	def post_process(self):
+		super(Check, self).post_process()
+		command_name = self._attributes['command']
+		command = self._config.get(("commands", command_name))
+		if command is None:
+			raise SammException("Command %s defined in check %s doesn't exist" % 
+				(self.command, self.name))
+		command.post_process()
+		self._command = command
+		self._applied_templates = True
 
-    def dict(self):
-        output = self._attributes.copy()
-        output['_command'] = self._command.dict()
-        output['register'] = self.register
-        return output
+	@property
+	def __dict__(self):
+		output = super(Check, self).__dict__
+		output['_command'] = self._command.__dict__
+		return output
