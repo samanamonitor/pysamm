1a79bb15de2f84768ec3e8ead10a5df0ee84c240
Fabian Baena <fabian.baena@samanagroup.com>
2024-05-08 09:57:37 -0400

    reassign latest tag from old image to new

--- support/build-image.sh
+++ support/build-image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 VERSION=$(PYTHONPATH=$(pwd)/.. python3 -c "import samm; print(samm.__version__)")
-docker build --no-cache -t samm-server:$VERSION samm-server
+docker rmi samm-server:latest
+docker build --no-cache -t samm-server:$VERSION -t samm-server:latest samm-server
