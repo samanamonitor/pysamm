8dea2f5c70aaa93e1951301a66abcf46f1fcb526
Fabian Baena <fabian.baena@samanagroup.com>
2024-09-05 10:55:57 -0400

    consolidated into only two queues: 'orders' and 'reports' and added type of message on both for future expansion; fixed bugs

--- samm/collector.py
+++ samm/collector.py
@@ -15,45 +15,60 @@ class Collector:
 		log.setLevel(self.config.get("debug"))
 		logging.basicConfig(stream=sys.stderr)
 		self._mq_server = self.config.get("mq.server", default='mq')
-		self._mq_queue_name = self.config.get("mq.queue_name", default="samm_attempts")
-		self._mq_queue_report_up = self.config.get("mq.queue_report_up", default="samm_report_up")
-		self._mq_queue_report_done = self.config.get("mq.queue_report_done", default="samm_report_done")
+		self._mq_queue_orders = self.config.get("mq.queue_orders", default="samm_orders")
+		self._mq_queue_reports = self.config.get("mq.queue_reports", default="samm_reports")
 		self._mq_queue_ttl = self.config.get("mq.queue_ttl", default=60000)
 		self.attempt_dict = {}
 		self.collecting = False
 		self.connection = None
 		self._delivery_tag = None
+		self.collection_count = 0
 
 	def on_open(self, connection):
 		connection.channel(on_open_callback=self.on_channel_open)
 
 	def on_open_error_callback(self, connection, err):
 		log.exception(err)
-		connectin.close()
+		self.connection.close()
 		sys.exit(1)
 
 	def on_close_callback(self, connectin, err):
 		log.exception(err)
-		connection.close()
+		self.connection.close()
 		sys.exit(0)
 
 	def on_channel_open(self, channel):
 
 		def message_callback(ch, method, properties, body):
-			key = json.loads(body)
+			order = json.loads(body)
+			if 'type' not in order:
+				raise Exception(f"Invalid order. Missing 'type'. {body}")
+			order_type = order.pop('type')
 			log.debug(f" [x] Received {body} {method.delivery_tag}")
-
-			if not self.attempt_dict[key].collect():
-				log.debug(f" [x] Rejected message {body} {method.delivery_tag}")
+			if order_type == "collect":
+				result = collect(order)
+			if result:
+				self._delivery_tag = method.delivery_tag
+				log.debug(f" [x] Accepted {body} {method.delivery_tag}")
+			else:
 				ch.basic_reject(delivery_tag = method.delivery_tag)
-				return
-			log.debug(f" [x] Accepted {body} {method.delivery_tag}")
-			self._delivery_tag = method.delivery_tag
+				log.debug(f" [x] Rejected message {body} {method.delivery_tag}")
+
+		def collect(order):
+			try:
+				attempt_name = order['attempt_name']
+			except Exception as e:
+				log.error(f"Invalid input from scheduler {data}")
+				log.exception(e)
+				return False
+
+			return self.attempt_dict[attempt_name].collect()
 
 		def up_check_callback(instance_name, value):
 			channel.basic_publish(exchange='', 
-					routing_key=self._mq_queue_report_up, 
+					routing_key=self._mq_queue_reports,
 					body=json.dumps({
+						"type": "up_check",
 						"attempt_name": instance_name,
 						"value": value
 						}))
@@ -62,24 +77,23 @@ class Collector:
 		def done_callback(data):
 			channel.basic_ack(delivery_tag = self._delivery_tag)
 			channel.basic_publish(exchange='', 
-					routing_key=self._mq_queue_report_done, 
+					routing_key=self._mq_queue_reports,
 					body=json.dumps({
+						"type": "done",
 						"attempt_name": data
 						}))
 			log.debug(f" [v] Collecting done by {str(data)}")
+			self.collection_count += 1
 
 
-		channel.queue_declare(queue=self._mq_queue_name,
+		channel.queue_declare(queue=self._mq_queue_orders,
 			arguments={'x-message-ttl' : self._mq_queue_ttl})
 
-		channel.queue_declare(queue=self._mq_queue_report_up,
-			arguments={'x-message-ttl' : self._mq_queue_ttl})
-
-		channel.queue_declare(queue=self._mq_queue_report_done,
+		channel.queue_declare(queue=self._mq_queue_reports,
 			arguments={'x-message-ttl' : self._mq_queue_ttl})
 
 		channel.basic_qos(prefetch_count=1)
-		channel.basic_consume(queue=self._mq_queue_name, on_message_callback=message_callback, auto_ack=False)
+		channel.basic_consume(queue=self._mq_queue_orders, on_message_callback=message_callback, auto_ack=False)
 		start_exporter(self, up_check_callback=up_check_callback, done_callback=done_callback, external_scheduler=True)
 
 	def run(self):
