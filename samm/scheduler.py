18c6958bd141323a2a8ce1d9a8a21710da1297c6
Fabian Baena <fabian.baena@samanagroup.com>
2024-09-05 10:57:40 -0400

    consolidated into only two queues: 'orders' and 'reports' and added type of message on both for future expansion; fixed bugs

--- samm/scheduler.py
+++ samm/scheduler.py
@@ -22,11 +22,10 @@ class Scheduler:
 		self._debug_level = self._config.get("debug", default="INFO")
 		log.setLevel(self._debug_level)
 		logging.basicConfig(stream=sys.stderr)
-		self._loop_sleep = self._config.get("loop_sleep", default=5)
+		self._loop_sleep_s = self._config.get("loop_sleep_s", default=5)
 		self._mq_server = self._config.get("mq.server", default='mq')
-		self._mq_queue_name = self._config.get("mq.queue_name", default="samm_attempts")
-		self._mq_queue_report_up = self._config.get("mq.queue_report_up", default="samm_report_up")
-		self._mq_queue_report_done = self._config.get("mq.queue_report_done", default="samm_report_done")
+		self._mq_queue_orders = self._config.get("mq.queue_orders", default="samm_orders")
+		self._mq_queue_reports = self._config.get("mq.queue_reports", default="samm_reports")
 		self._mq_queue_ttl = self._config.get("mq.queue_ttl", default=60000)
 		self._pending_retry = self._config.get("pending_retry", default=60)
 		self.channel = None
@@ -58,13 +57,25 @@ class Scheduler:
 		log.debug("Channel opened.")
 		self.channel = channel
 
-		def report_up_callback(ch, method, properties, body):
-			data = json.loads(body)
+		def reports_callback(ch, method, properties, body):
+			report = json.loads(body)
+			if "type" not in report:
+				raise Exception(f"Invalid report. Missing 'type'. {body}")
+			report_type = report.pop('type')
+			if report_type == 'up_check':
+				report_up(report)
+			elif report_type == 'done':
+				report_done(report)
+			else:
+				log.warning(f"Unknown report type {report_type}. Data={report}")
+			ch.basic_ack(delivery_tag = method.delivery_tag)
+
+		def report_up(data):
 			try:
 				attempt_name = data['attempt_name']
 				value = int(data['value'])
 			except Exception as e:
-				log.error(f"Invalid input from collector {body}")
+				log.error(f"Invalid input from collector {data}")
 				log.exception(e)
 				return
 			attempt = self.attempt_dict.get(attempt_name)
@@ -87,27 +98,19 @@ class Scheduler:
 						if attempt.instance.up_check_name == attempt.check.name:
 							continue
 						attempt.schedule(0)
+			log.debug(" [x] Received instance_up(%s)" % (str(data)))
 
-			log.debug(" [x] Received instance_up(%s)" % (str(body)))
-			ch.basic_ack(delivery_tag = method.delivery_tag)
+		def report_done(data):
+			log.debug(" [x] Received done(%s)'" % (str(data)))
 
-		def done_callback(ch, method, properties, body):
-			data = json.loads(body)
-
-			log.debug(" [x] Received done(%s)'" % (str(body)))
-			ch.basic_ack(delivery_tag = method.delivery_tag)
-
-		channel.queue_declare(queue=self._mq_queue_name,
+		channel.queue_declare(queue=self._mq_queue_orders,
 			arguments={'x-message-ttl' : self._mq_queue_ttl})
 
-		channel.queue_declare(queue=self._mq_queue_report_up,
+		channel.queue_declare(queue=self._mq_queue_reports,
 			arguments={'x-message-ttl' : self._mq_queue_ttl})
 
-		channel.queue_declare(queue=self._mq_queue_report_done,
-			arguments={'x-message-ttl' : self._mq_queue_ttl})
 
-		channel.basic_consume(queue=self._mq_queue_report_up, on_message_callback=report_up_callback, auto_ack=False)
-		channel.basic_consume(queue=self._mq_queue_report_done, on_message_callback=done_callback, auto_ack=False)
+		channel.basic_consume(queue=self._mq_queue_reports, on_message_callback=reports_callback, auto_ack=False)
 
 	def order_attempt(self, attempt):
 		if not attempt.due():
@@ -125,11 +128,14 @@ class Scheduler:
 				log.debug(f"Instance {instance.name} is PENDING skipping {attempt.name}")
 				attempt.schedule(attempt.pending_retry)
 				return
-		self.channel.basic_publish(exchange='', 
-			routing_key=self._mq_queue_name, 
-			body=json.dumps(attempt.name))
+		self.channel.basic_publish(exchange='',
+			routing_key=self._mq_queue_orders,
+			body=json.dumps({
+				"type": "collect",
+				"attempt_name": attempt.name
+				}))
 		attempt.schedule_next()
-		log.debug(" [x] Queued attempt(%s)'" % (attempt.name))
+		log.debug(" [x] Queued collect attempt(%s)'" % (attempt.name))
 
 	def process_loop(self):
 		if len(self.attempt_dict.keys()) < 1:
@@ -141,16 +147,16 @@ class Scheduler:
 
 
 		try:
-			loop_sleep = 1
+			loop_sleep_s = 1
 			while self._keep_running:
-				_ = connection.ioloop.call_later(loop_sleep, connection.ioloop.stop)
+				_ = connection.ioloop.call_later(loop_sleep_s, connection.ioloop.stop)
 				connection.ioloop.start()
 				while self.channel is None:
 					log.debug("MQ not ready. Waiting.")
 					time.sleep(1)
 				for _, attempt in self.attempt_dict.items():
 						self.order_attempt(attempt)
-				loop_sleep = self._loop_sleep
+				loop_sleep_s = self._loop_sleep_s
 
 		except KeyboardInterrupt:
 			pass
